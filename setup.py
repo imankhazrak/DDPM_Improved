3f5d7c47079aefbe7c1e5eaeb51c2c312feb6c89
Iman Khazrak <iman.khazrak.ik@gmail.com>
2024-02-24 14:26:47 -0500

    modify setup.py and adding tiny imagenet

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="improved-diffusion",
     py_modules=["improved_diffusion"],
+    packages=find_packages(),
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
